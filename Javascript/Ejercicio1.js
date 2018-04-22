3dc2c95c42405b92816412d2624371320ff125be
JorgeLuis1395 <carrillojorge1395@hotmail.com>
2018-04-22 17:52:44 -0500

    Ejercicio 1

---
+++ Javascript/Ejercicio1.js
@@ -0,0 +1,12 @@
+var arreglo = [ 5,3,'s','r',3,'b','u',true,undefined,null,new Date(),{nombre:'Jorge'},[12,12],'q',];
+let arregloString = [];
+var arregloDesecho=[];
+function validarNumeroDeStringsEnArreglo(arreglo) {
+    arreglo.forEach( function(valor) {
+        if ((typeof valor== 'string')) {
+            arregloString+=valor;
+        }else arregloDesecho+=valor;
+    });
+    return arregloString.length;
+}
+console.log(validarNumeroDeStringsEnArreglo(arreglo));
